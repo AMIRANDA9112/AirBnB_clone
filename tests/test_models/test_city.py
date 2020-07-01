5eb6d406bb64a60dcb01e16affad231c4958b279
AMIRANDA9112 <andreselfm@gmail.com>
2020-07-01 13:29:03 -0500

    some issues and test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,43 +7,43 @@ from models.user import User
 
 
 class TestBase_Model(unittest.TestCase):
-    """Tests for class Place"""
+    """Tests for class City"""
 
     def test_docstring(self):
         '''test if funcions, methods, classes
         and modules have docstring'''
         msj = "Módulo does not has docstring"
-        self.assertIsNotNone(models.Place.__doc__, msj)  # Modules
+        self.assertIsNotNone(models.City.__doc__, msj)  # Modules
         msj = "Clase does not has docstring"
-        self.assertIsNotNone(Place.__doc__, msj)  # Classes
+        self.assertIsNotNone(City.__doc__, msj)  # Classes
 
     def test_executable_file(self):
         '''test if file has permissions u+x to execute'''
         # Check for read access
-        is_read_true = os.access('models/place.py', os.R_OK)
+        is_read_true = os.access('models/city.py', os.R_OK)
         self.assertTrue(is_read_true)
         # Check for write access
-        is_write_true = os.access('models/place.py', os.W_OK)
+        is_write_true = os.access('models/city.py', os.W_OK)
         self.assertTrue(is_write_true)
         # Check for execution access
-        is_exec_true = os.access('models/place.py', os.X_OK)
+        is_exec_true = os.access('models/city.py', os.X_OK)
         self.assertTrue(is_exec_true)
 
     def test_is_an_instance(self):
         '''check if my_model is an instance of User'''
-        my_model = Palce()
-        self.assertIsInstance(my_model, Place)
+        my_model = City()
+        self.assertIsInstance(my_model, City)
 
     def test_id(self):
         '''test if the id of two instances are different'''
-        my_model = Place()
-        my_model1 = Place()
+        my_model = City()
+        my_model1 = City()
         self.assertNotEqual(my_model.id, my_model1.id)
 
     def test_save(self):
         '''check if the attribute updated_at (date) is updated for
         the same object with the current date'''
-        my_model2 = Place()
+        my_model2 = City()
         first_updated = my_model2.updated_at
         my_model2.save()
         second_updated = my_model2.updated_at
@@ -53,12 +53,12 @@ class TestBase_Model(unittest.TestCase):
         '''check if to_dict returns a dictionary, if add a class
         key with class name of the object and if updated_at and
         created_at are converted to string object in ISO format.'''
-        my_model3 = Place()
+        my_model3 = City()
         my_dict_model3 = my_model3.to_dict()
         self.assertIsInstance(my_dict_model3, dict)
         for key, value in my_dict_model3.items():
             flag = 0
-            if my_dict_model3['__class__'] == 'Placee':
+            if my_dict_model3['__class__'] == 'City':
                 flag += 1
             self.assertTrue(flag == 1)
         for key, value in my_dict_model3.items():
