6649de286e2c7ce31e25f78f9b4f1f62a721bced
Camilo Moncada <monk-da@hotmail.com>
2020-07-01 00:07:53 -0500

    add test storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,9 +3,21 @@
 import os
 import unittest
 import models
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
 class TestFile_Storage(unittest.TestCase):
-    '''start tests'''
-    pass
+    '''tests Storage'''
+
+    def test_1(self):
+        """  Test Dictionary """
+        model = BaseModel()
+        model.save()
+        new_object = models.storage.all()
+        self.assertEqual(dict, type(new_object))
+
+    def test_2(self):
+        '''check if my_model is an instance of BaseModel'''
+        my_model = FileStorage()
+        self.assertIsInstance(my_model, FileStorage)
